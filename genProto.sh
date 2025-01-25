0675d6265dc32e86bad0e7a9cd5b9f4b44638eb8
2soumen.d@gmail.com <2soumen.d@gmail.com>
2025-01-25 14:21:48 -0600

    enhanced

--- genProto.sh
+++ genProto.sh
@@ -1,3 +1,12 @@
 SRC_DIR=proto
 DST_DIR=users-java
-protoc -I=$SRC_DIR --java_out=$DST_DIR $SRC_DIR/users.proto
+SWAGGER=openapi
+rm -fr $DST_DIR
+rm -fr $SWAGGER
+mkdir -p $DST_DIR
+mkdir -p $SWAGGER
+
+protoc -I=$SRC_DIR \
+    --java_out=$DST_DIR \
+    --openapi_out=$SWAGGER \
+    $SRC_DIR/users.proto
